67be9a5bc54d76bb822fbe154cccade0555fde57
unknown <nguyentandangkhuong2912@gmail.com>
2025-06-17 11:15:44 +0700

    create project

--- src/main/java/com/ntdk/quizapp/PrimaryController.java
+++ src/main/java/com/ntdk/quizapp/PrimaryController.java
@@ -1,12 +1,14 @@
 package com.ntdk.quizapp;
 
-import java.io.IOException;
-import javafx.fxml.FXML;
+import com.ntdk.utils.MyAlert;
+import javafx.event.ActionEvent;
 
 public class PrimaryController {
-
-    @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    public void handleQuestionManagement(ActionEvent event){
+        MyAlert.getInstance().showMsg("QuestionManagement: Coming soon ...");
+        
+    }
+    public void handlePractice(ActionEvent evnet){
+        MyAlert.getInstance().showMsg("Practice: Coming soon ...");
     }
 }
